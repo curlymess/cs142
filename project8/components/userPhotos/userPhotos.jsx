f6c17e043084a99a42888fb6755803b84ff32047
Noor Fakih <nfakih@stanford.edu>
2023-03-13 19:56:26 -0700

    decomposed and created photocard

--- project8/components/userPhotos/userPhotos.jsx
+++ project8/components/userPhotos/userPhotos.jsx
@@ -18,6 +18,7 @@ import BookmarkIcon from '@mui/icons-material/Bookmark';
 
 import axios from 'axios';
 import NewComment from './NewComment';
+import PhotoCard from './photoCard';
 
 /**
  * Define UserPhotos, a React componment of CS142 project #5
@@ -33,8 +34,8 @@ class UserPhotos extends React.Component {
 		};
 		this.handleBack = this.handleBack.bind(this);
 		this.handleNext = this.handleNext.bind(this);
-		this.checkIfFav = this.checkIfFav.bind(this);
 		this.source = axios.CancelToken.source();
+
 	}
 
 	componentDidMount() {
@@ -84,23 +85,7 @@ class UserPhotos extends React.Component {
 
 	handleBack() { this.setState((prevState) => ({ step: prevState.step - 1 })); }
 
-	convertTime(time) {
-		let pos = time.indexOf('.');
-		let replaced = time.replace(/[A-Z]/g, function () {
-			return " ";
-		});
-		return replaced.slice(0, pos);
-	}
-
-	checkIfFav(photo_id) {
-		for (let i = 0; i < this.state.favorites.length; i++) {
-			if (this.state.favorites[i]._id === photo_id) {
-				return true;
-			}
-		}
-		return false
-	}
-
+	// for favorite / bookmarking
 	updateFav = () => {
 		console.log("in updatefac");
 		axios.get(`/favorites`).then(response => {
@@ -112,154 +97,26 @@ class UserPhotos extends React.Component {
 		});
 	}
 
-	removeFavFromArray(photo_id) {
-		const newArray = [];
-		for (let i = 0; i < this.state.favorites.length; i++) {
-			if (this.state.favorites[i]._id !== photo_id) {
-				newArray.push(this.state.favorites[i]);
-			}
-		}
-		this.setState({ favorites: newArray });
-	};
-
-	handleFavoriteClick = (photo_id) => {
-		if (checkIfFav(photo_id)) {
-			axios.delete(`/favorite/${photo_id}`, {})
-				.then(response => {
-					this.removeFavFromArray(photo_id);
-					console.log(response.data);
-				})
-				.catch(error => {
-					console.log(error.response.data);
-				});
-		} else {
-			axios.post(`/favorite/${photo_id}`)
-				.then(response => {
-					var newArray = this.state.favorites;
-					newArray.push(photo_id);
-					this.setState({
-						favorites: newArray,
-					});
-					console.log(response.data);
-				})
-				.catch(error => {
-					console.log(error.response.data);
-				});
-		}
-	};
-
-
 	render() {
 		return (
-			<Grid>
-				{(this.state.userPhotos.map(
-					(photo, index) => (
-						<Grid item key={photo._id} >
-							<Card >
-								{Math.abs(this.state.step - index) < 1 ? (
-									// start of card
-									<div>
-										{/* poster */}
-										<CardHeader
-											avatar={(<Avatar sx={{ backgroundColor: "#DFFFD8" }}> <Face2Icon sx={{ color: "#95BDFF" }} /> </Avatar>)}
-											title={(
-												<Link to={`/users/${this.state.user._id}`} style={{ textDecoration: 'none' }}>
-													<Typography variant="h4">{`${this.state.user.first_name} ${this.state.user.last_name}`}</Typography>
-												</Link>
-											)}
-											subheader={(
-												<Typography variant="h7">
-													{`${this.convertTime(photo.date_time)}`}
-												</Typography>
-											)}
-											sx={{ backgroundColor: "#F7c8e0" }} />
-										{/* image */}
-										<CardMedia component='img' image={`/images/${photo.file_name}`} alt={photo._id} />
-										{/* scroll images */}
-										<MobileStepper
-											steps={this.state.userPhotos.length}
-											activeStep={this.state.step}
-											position='static'
-											nextButton={(
-												<Button size='small' onClick={this.handleNext} disabled={this.state.step === this.state.userPhotos.length - 1}>
-													{' '} Next <KeyboardArrowRight />
-												</Button>
-											)}
-											backButton={(
-												<Button size='small' onClick={this.handleBack} disabled={this.state.step === 0}><KeyboardArrowLeft /> Back
-												</Button>
-											)} />
-										{/* action bar */}
-										<CardActions sx={{ display: "flex", justifyContent: "space-between" }}>
+			this.state.userPhotos ?
+				this.state.userPhotos.map((photo, index) => {
+					let isFav = false;
+					for (let i = 0; i < this.state.favorites.length; i++) {
+						if (this.state.favorites[i]._id === photo._id) {
+							isFav = true;
+							break;
+						}
+					}
+					return (
+						<PhotoCard key={photo._id} userPhotoIndex={index} isFav={isFav}
+							handleBack={this.handleBack} handleNext={this.handleNext} step={this.state.step}
+							photo={photo} user={this.state.user} userPhotos={this.state.userPhotos}
+						/>
+					);
 
-											{/* if array favorites has the photo id of this photo then true */}
-											{this.checkIfFav(photo._id) ?
-												<Button size="small" onClick={this.updateFav(photo._id)}>
-													<BookmarkIcon />
-												</Button>
-												:
-												<Button size="small">
-													<BookmarkBorderIcon />
-												</Button>
-											}
+				}) : null
 
-
-											<NewComment currPhotoId={photo._id} currPhotoFileName={photo.file_name} currUser={this.state.user.first_name + ' ' + this.state.user.last_name} />
-										</CardActions>
-										{/* the comments */}
-										{photo.comments ?
-											// comment section
-											(
-												<div>
-													<Typography variant='h4' sx={{ paddingLeft: "5px" }}>Comment Section</Typography>
-													<List
-														sx={{
-															width: '100%',
-															bgcolor: 'background.paper',
-															position: 'relative',
-															overflow: 'auto',
-															maxHeight: 200,
-														}}
-													>
-														{photo.comments.map((comment) => (
-															<ListItem divider={true} key={comment._id} className='comment-section' sx={{ backgroundColor: "#fbe3ef", display: 'flex', flexDirection:'column', alignItems:'flex-start' }}>
-																<div className="commentHeader">
-																	<Link to={`/users/${comment.user._id}`} style={{ textDecoration: 'none' }}>
-																		<ListItemAvatar sx={{ display: { xs: 'none', sm: 'flex' } }}>
-																			<Avatar sx={{ backgroundColor: "#DFFFD8" }}> <Face2Icon sx={{ color: "#95BDFF" }} /> </Avatar>
-																		</ListItemAvatar>
-																	</Link>
-																	<ListItemText primary={(
-																		<div>
-																			<Link to={`/users/${comment.user._id}`} style={{ textDecoration: 'none' }}>
-																				<Typography variant="h4">{`${comment.user.first_name} ${comment.user.last_name}`}</Typography>
-																			</Link>
-																			<Typography variant="h7">{`${this.convertTime(photo.date_time)}`}</Typography>
-																		</div>
-																	)} />
-																</div>
-																<Typography variant="body1">{comment.comment}</Typography>
-															</ListItem>
-														))}
-													</List>
-												</div>
-											) :
-											<Typography variant="body1" >
-												no comments
-											</Typography>
-										}
-										{/* end of comment section */}
-									</div>
-									// end of card
-
-								) : null}
-
-							</Card>
-						</Grid>
-
-
-					)))}
-			</Grid>
 		); // end of return
 	} // end of render
 }
